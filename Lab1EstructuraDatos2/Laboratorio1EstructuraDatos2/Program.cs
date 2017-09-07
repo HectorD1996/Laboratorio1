a2726c2e130d78e49d1df3fb01a441131b8468b3
Hector Fernando Davila Lemus <hectorfdl@hotmail.com>
2017-09-07 08:03:25 -0600

    Console Applicaction

---
+++ Lab1EstructuraDatos2/Laboratorio1EstructuraDatos2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1EstructuraDatos2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
