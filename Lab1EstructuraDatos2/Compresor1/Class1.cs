c7f247ac9640677ce615f3c450a121cfc4356785
sagramos013 <sagramos_13@hotmail.com>
2017-09-12 09:12:52 -0600

    modifique la clase "Compresor1"
    le agregue un par de metodos

--- Lab1EstructuraDatos2/Compresor1/Class1.cs
+++ Lab1EstructuraDatos2/Compresor1/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,21 @@ namespace Compresor1
 {
     public class Class1
     {
+    	//Cambiar el nombre de la clase despues...
+
+    	//path -> direccion del archivo
+    	//tipo -> metodo de comprecion que se utilizara
+    	//		  true  -> RLN
+    	//		  false -> Huffman
+    	public void comprimir(string path, bool tipo)
+    	{
+    	}
+    	public void descomprimir(string path, bool tipo)
+    	{
+     	}
+
+
+     	private void mostrarResultados()
+     	{}
     }
 }
